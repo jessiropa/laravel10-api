815949605b86639acb9398d22f4f61628f6e9da1
jessiropa <kristinjessi9700@gmail.com>
2025-09-04 20:21:13 +0700

    insert data menggunakan rest api di dalam laravel

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 // import resource postresource
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -15,5 +16,29 @@ class PostController extends Controller
     public function index(){
         $posts = Post::latest()->paginate(5);
         return new PostResource(true, 'List Data Posts', $posts);   
+    }
+
+    public function store(Request $request){        
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);   
     }
 }
